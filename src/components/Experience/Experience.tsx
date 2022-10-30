6cac450a10d2780b87118dc54e3563b4085f8951
voroshilova <voroshilova.av@yandex.ru>
2022-10-30 21:10:37 +0400

    feat: add experience section

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -5,7 +5,59 @@ const Experience = () => {
   return (
     <section id="experience">
       <h3>Work experience</h3>
-      <section className="experience__wrapper"></section>
+      <section className="experience-wrapper">
+        <div className="experience-wrapper__info">
+          <p>2019-2022</p>
+          <div className="experience-wrapper__bar"></div>
+          <p className="experience-wrapper__info_bold">REG.RU</p>
+          <p>Samara, Russia</p>
+        </div>
+        <div className="experience-wrapper__info">
+          <p className="experience-wrapper__info_bold">
+            Technical support specialist (web-hosting issues)
+          </p>
+          <ul>
+            <li>
+              <span>&#10004;</span> Site hosting, work with Apache, Nginx, Mysql and PHP
+            </li>
+            <li>
+              <span>&#10004;</span> Troubleshooting server errors and work with the command line
+            </li>
+            <li>
+              <span>&#10004;</span> Installing and configuring the software, including the CMS
+            </li>
+            <li>
+              <span>&#10004;</span> Mail setup, routing and connection troubleshooting
+            </li>
+            <li>
+              <span>&#10004;</span> Creating bug cards when problems are detected
+            </li>
+            <li>
+              <span>&#10004;</span> Monitoring hosting servers
+            </li>
+          </ul>
+        </div>
+      </section>
+      <hr></hr>
+      <section className="experience-wrapper">
+        <div className="experience-wrapper__info">
+          <p>2018-2019</p>
+          <div className="experience-wrapper__bar"></div>
+          <p className="experience-wrapper__info_bold">Samara State Aerospace University</p>
+          <p>Samara, Russia</p>
+        </div>
+        <div className="experience-wrapper__info">
+          <p className="experience-wrapper__info_bold">Engineer</p>
+          <ul>
+            <li>
+              <span>&#10004;</span> Creating documentation of expert opinions on university exhibits
+            </li>
+            <li>
+              <span>&#10004;</span> Working at exhibitions with scientific exhibits
+            </li>
+          </ul>
+        </div>
+      </section>
     </section>
   );
 };
