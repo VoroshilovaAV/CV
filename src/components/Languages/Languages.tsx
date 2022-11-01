409ad69e5df9df1f11ae0c4d854aa827dab82a55
voroshilova <voroshilova.av@yandex.ru>
2022-11-01 22:20:48 +0400

    feat: add languages section

--- src/components/Languages/Languages.tsx
+++ src/components/Languages/Languages.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { ReactComponent as En } from '../../assets/icons/en.svg';
+import { ReactComponent as Ru } from '../../assets/icons/ru.svg';
 import './index.scss';
 
 const Languages = () => {
   return (
     <section id="languages">
       <h3>Languages</h3>
+      <section className="languages-wrapper">
+        <div className="languages-wrapper__ru">
+          <Ru />
+          <h4>Russian</h4>
+          <p>( Native )</p>
+        </div>
+        <div className="languages-wrapper__en">
+          <En />
+          <h4>English</h4>
+          <p>( Intermediate )</p>
+        </div>
+      </section>
     </section>
   );
 };
