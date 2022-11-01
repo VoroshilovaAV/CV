4ccc7119a65f01dde7447fcec1c623a1efc0482f
voroshilova <voroshilova.av@yandex.ru>
2022-11-01 23:20:19 +0400

    feat: add a button to scroll up

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AboutMe from '../AboutMe/AboutMe';
 import Skills from '../Skills/Skills';
 import Projects from '../Projects/Projects';
@@ -8,6 +8,22 @@ import Languages from '../Languages/Languages';
 import './index.scss';
 
 const Main = () => {
+  const [showButton, setShowButton] = useState(false);
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false);
+    };
+    window.addEventListener('scroll', handleScrollButtonVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollButtonVisibility);
+    };
+  }, []);
+
   return (
     <main>
       <div className="main-wrapper">
@@ -17,6 +33,13 @@ const Main = () => {
         <Experience />
         <Education />
         <Languages />
+        {showButton && (
+          <div className="main-wrapper__scrollToTop">
+            <button className="main-wrapper__button" onClick={handleScrollToTop}>
+              &#8679;
+            </button>
+          </div>
+        )}
       </div>
     </main>
   );
